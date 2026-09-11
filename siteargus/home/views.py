bc3391626fba39357737dde9af1008e9c1138a6f
heitosena <heitorsena83@gmail.com>
2026-09-11 15:39:20 -0300

    criptografia da senha, token CSRF e melhoria das views

--- siteargus/home/views.py
+++ siteargus/home/views.py
@@ -4,4 +4,4 @@ from django.http import HttpResponse
 # Create your views here.from django.http import HttpResponse
 
 def index(request):
-    return  HttpResponse("Olá, bem-vindo ao site argus!")
+    return render(request, "home/index.html")
--- siteargus/usuarios/views.py
+++ siteargus/usuarios/views.py
@@ -1,15 +1,34 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.contrib.auth.hashers import make_password
 
 # Create your views here.
 def login_views(request):
     return render(request, "usuarios/login.html")
 
+# Método de armazenar as informações de cadastro de usuario (ainda sem conectar no MySQL)...
 def cadastro_views(request):
-    return HttpResponse("Pagina de cadastro")
+
+    if request.method == "POST":
+        nome = request.POST.get("nome")
+        email = request.POST.get("email")
+        telefone = request.POST.get("telefone")
+        senha = request.POST.get("senha")
+
+        senha = make_password(senha)
+
+        print(nome)
+        print(email)
+        print(telefone)
+        print(senha)
+
+    return render(request, "usuarios/cadastro.html")
 
 def logout_views(request):
-    return HttpResponse("Pagina de logout")
+    return render(request, "usuarios/logout.html")
 
 def dashboard_views(request):
-    return HttpResponse("Pagina de Dashboard")
+    return render(request, "usuarios/dashboard.html")
+
+def cadastrodisp_views(request):
+    return render(request, "usuarios/cadastrodisp.html")
