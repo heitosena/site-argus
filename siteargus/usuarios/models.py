c70f171e87d01a48e31c5cbe1f22712f25546cda
heitosena <heitorsena83@gmail.com>
2026-09-11 16:52:51 -0300

    adicionando models usuario

--- siteargus/usuarios/models.py
+++ siteargus/usuarios/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class usuario(models.Model):
+    nome = models.CharField(max_length=120)
+    email = models.CharField(max_length=150)
+    telefone = models.CharField(max_length=20)
+    senha =models.CharField(max_lenght=255)
